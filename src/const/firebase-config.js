50319e2f6d6fe03d689c3816ee63a917e3bf3a9a
yagisuke <yagisuke.0209@gmail.com>
2018-04-10 02:48:10 +0900

    firebase config

---
+++ src/const/firebase-config.js
@@ -0,0 +1,12 @@
+/**
+ * NOTE: firebaseのconfig情報に変更してください
+ */
+
+export default {
+    apiKey: 'apiKey',
+    authDomain: 'authDomain',
+    databaseURL: 'databaseURL',
+    projectId: 'projectId',
+    storageBucket: 'storageBucket',
+    messagingSenderId: 'messagingSenderId'
+}
