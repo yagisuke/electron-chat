493d19509f806d8b7ffd5dfd5e3c5de1ef5017e5
yagisuke <yagisuke.0209@gmail.com>
2018-04-10 15:03:42 +0900

    add Signin page

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
-import * as firebase from 'firebase'
+import firebase from 'firebase'
 import Const from '../const'
 import Signin from './Signin'
 import Signup from './Signup'
@@ -9,6 +9,9 @@ import Rooms from './Rooms'
 import Room from './Room'
 
 const { FIREBASE_CONFIG } = Const
+
+// firebaseの初期化
+firebase.initializeApp(FIREBASE_CONFIG)
 
 // routingの定義
 const appRouting = (
@@ -22,9 +25,6 @@ const appRouting = (
         </Switch>
     </HashRouter>
 )
-
-// firebaseの初期化
-firebase.initializeApp(FIREBASE_CONFIG)
 
 render(
     appRouting,
